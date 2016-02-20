04d4eec335b597ea59c156df1f1361b5a767a286
Chris Yuska <chrisyuska@gmail.com>
2016-02-19 21:42:35 -0500

    playing with gradually shaded splash image to cheaply immitate day/night

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+var canvas = $("canvas#shade");
+var ctx = canvas.get(0).getContext("2d");
+var initialShade = 0.4;
+var shade = initialShade;
+var fps = 100;
+var interval = null;
+var step = 0.001;
+var up = false;
+
 $(function() {
   // init smooth scrolling of inner-page links
   $('a[href*="#"]:not([href="#"])').click(function() {
@@ -16,4 +25,30 @@ $(function() {
   // set height of splash and offset of content wrapper so mobile url bar doesn't cause resizing on scroll
   $(".splash-container").outerHeight($(".splash-container").outerHeight());
   $(".content-wrapper").css("top", $(".content-wrapper").offset().top);
+
+  // transition splash image brightness in slowly
+  initDrawShade();
 });
+
+function initDrawShade() {
+  interval = window.setInterval(drawShade, 1000 / fps);
+}
+
+function drawShade() {
+  shade = shade + (up ? 1 : -1) * step;
+  if (shade < 0) {
+    window.clearInterval(interval);
+    up = true;
+    interval = window.setTimeout(initDrawShade, 6000);
+  }
+
+  if (shade > 0.6) {
+    window.clearInterval(interval);
+    up = false;
+    interval = window.setTimeout(initDrawShade, 6000);
+  }
+
+  ctx.clearRect(0, 0, canvas.outerWidth(), canvas.outerHeight());
+  ctx.fillStyle = 'rgba(0,0,0,' + shade + ')';
+  ctx.fillRect (0, 0, canvas.outerWidth(), canvas.outerHeight());
+}
